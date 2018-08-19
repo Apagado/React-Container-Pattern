9a4bc9d5cf7bee038bdb1c0dc95f821f0373fd70
ext.joao.silva.lopes.mindera <joao.silva.lopes@mindera.com>
2018-08-19 16:29:05 +0100

    Added Container Pattern Example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'whatwg-fetch';
+import styles from './index.css';
 
-class App extends React.Component {
+//VIEW
+const UserList = ({ users }) => (
+  <ul className={styles.userList}>
+    { users.map(({name, id, gender})=><li key={ id } className={styles[`userListItem--${gender}`]}>{ name }</li>) }
+  </ul>
+)
+
+//CONTAINER
+class UserListContainer extends React.Component {
+  
+  state = {
+    loading: true,
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=10')
+      .then(r=>r.json())
+      .then(({ results })=>this.setState({
+        users:  results.map(({name, gender, login: { uuid: id }})=>({name: this._formatName(name), gender, id})),
+        loading: false
+      }));
+  }
+
+  _formatName = ({title, first, last}) => `${title}. ${first} ${last}`;
+
   render() {
-    return <div/>;
+
+    if(this.state.loading) return <p>Loading...</p>
+
+    const { users } = this.state;
+    const userListProps = { ...this.props, users };
+
+    return <UserList { ...userListProps }/>;
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <UserListContainer />,
   document.getElementById('app')
 );
